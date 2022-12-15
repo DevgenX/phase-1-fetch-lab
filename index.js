3356430f3466b4b2301e6d6de8da7604766ed22f
devgenx <sebastian110420@gmail.com>
2022-12-15 13:22:46 -0500

    commit tree 75d5cedd2ada7604ddf372af99d7f85edb97e0f8

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => res.json())
+    .then((data) => renderBooks(data));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
+
+const numberofPages = (books) => {
+  const pages = document.querySelector("div");
+  books.forEach((book) => {
+    const page = document.createElement("h3");
+    page.textContent = book.numberOfPages;
+    pages.appendChild(page);
+  });
+};
